c19d847c4636d1a74f87d10369378e6c336e0403
Tiamo Minard <minardtiamo@gmail.com>
2023-04-04 23:07:17 +0000

    Chello World adding the bit stuff

--- ChelloWorld.C
+++ ChelloWorld.C
@@ -1,8 +1,9 @@
 #include <stdio.h>
 int main()
 {
-    printf("Chello World (Now selling Chellos at an UNREASONABLE price!)");
+    printf("Hello World! Did you know that the number of bytes in an int is %lu\n", sizeof(int));
+    printf("While the number of bytes in a short is %lu\n", sizeof(short));
     return 0;
-    }
+}
 
     //in case you forget, run this with ./ChelloWorld (the exe is "ChelloWorld.exe")
