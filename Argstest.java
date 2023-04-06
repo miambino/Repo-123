cfbf23df9580793673ae92d2b4af03322578e7f9
Tiamo Minard <minardtiamo@gmail.com>
2023-04-06 20:03:33 +0000

    Argstest added (test with # strings arg)

---
+++ Argstest.java
@@ -0,0 +1,11 @@
+class Argstest {
+    public static void main(String[] args)
+    {
+        System.out.println("Command-line arguments are");
+        
+        for(String str: args)
+        {
+            System.out.println(str);
+        }
+    }
+}
