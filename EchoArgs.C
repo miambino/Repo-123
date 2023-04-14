58e5103cd36f97e931f0a79465f70d7e1d29f48a
Tiamo Minard <minardtiamo@gmail.com>
2023-04-14 05:50:38 +0000

    echoargs.c annotated

---
+++ EchoArgs.C
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+        //argc is an integer that represents the number of arguments.
+        //argv is a pointer that points to a string (a char array) that stores the argument strings.
+
+    while           //while is a loop type in C. the format is "while(condition is met) {this happens;}"
+    (--argc > 0)      //--argc removes the command from the index of arguments, while loop depends on the condition that newargc > 0
+    printf
+    (
+        (argc > 0)
+        ?           //?: is the if-else in C. (condition) ? this : else, that
+        "%s": "%s", *(++argv)       //%s is the format specifier for string. if argc > 0, prints stored 
+    );
+    printf("\n");       //linebreak
+    return 0;
+}
